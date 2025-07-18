96e3603453c6231c6fe927234ac772e46502d6e2
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-18 14:08:13 +0300

    fix: seach imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import Pokemons from "./components/Pokemons";
+import Search from "./components/Search";
 import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Search />
+      <div className="flex justify-center mt-8 py-10 bg-slate-100">
+        <div className="grid grid-cols-6 gap-8">
+          <Pokemons />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/Pokemons.jsx
@@ -0,0 +1,25 @@
+import Pokecard from "./Pokecard";
+
+const Pokemons = ({ data }) => {
+    if(!data){
+        return <div>No pokemon to display yet</div>
+    }
+ else if (data.length === 0) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <>
+        {data.map((pokemon, index) => (
+          <div key={index}>
+            <Pokecard
+              id={pokemon.id}
+              image={pokemon.image}
+              Pokename={pokemon.Pokename}
+            />
+          </div>
+        ))}
+      </>
+    );
+  }
+};
+export default Pokemons;
