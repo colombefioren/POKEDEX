e00dce8c8573c541f2c109a5376ac9bf7577d8d0
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-19 21:01:51 +0300

    refactor: fetch pokemon data function to getPokemonList

--- src/lib/api/pokemonApi.js
+++ src/lib/api/pokemonApi.js
@@ -1,20 +1,23 @@
-export const fetchPokemonData = async (url) => {
+import { POKE_API } from "../config/api";
+
+export const getPokemonList = async (limit = null) => {
+  const url = limit
+    ? `${POKE_API.BASE_URL}${POKE_API.ENDPOINTS.POKEMON_LIMIT(limit)}`
+    : `${POKE_API.BASE_URL}${POKE_API.ENDPOINTS.POKEMON}`;
+
   try {
     const response = await fetch(url);
-
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-
     const data = await response.json();
-
     return data.map(({ pokedexId, name, image }) => ({
       id: pokedexId,
       name,
       image,
     }));
   } catch (error) {
-    console.error(`Error fetching pokemon data from ${url}:`, error);
+    console.error("Error fetching pokemon data:", error);
     throw error;
   }
 };
