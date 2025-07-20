0790bb31b57bb45a51433ef3e7af6904bcc3669c
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-20 09:24:20 +0300

    style: darker shade for pokeshell

--- src/components/PokedexShell.jsx
+++ src/components/PokedexShell.jsx
@@ -60,7 +60,7 @@ const PokedexShell = ({ children, loading }) => {
     <div className="relative w-full h-screen bg-black overflow-hidden">
       <div className="absolute inset-0 z-10 pointer-events-none">
         {/* top  */}
-        <div className="absolute top-0 left-0 right-0 h-16 bg-gradient-to-b from-gray-900 to-gray-800 border-b-2 border-gray-700">
+        <div className="absolute top-0 left-0 right-0 h-16 bg-gray-900 border-b-2 border-gray-700">
           <div className="flex justify-between items-center h-full">
             <img
               src="/assets/images/pokeballimage.png"
@@ -77,7 +77,7 @@ const PokedexShell = ({ children, loading }) => {
         </div>
 
         {/* bottom  */}
-        <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t from-gray-900 to-gray-800 border-t-2 border-gray-700">
+        <div className="absolute bottom-0 left-0 right-0 h-20 bg-gray-900 border-t-2 border-gray-700">
           <div className="flex justify-center items-center h-full px-6 space-x-8">
             <div className="w-24 h-6 bg-gray-700 rounded-full"></div>
           </div>
@@ -113,7 +113,7 @@ const PokedexShell = ({ children, loading }) => {
             duration: 1.5,
             ease: [0.22, 1, 0.36, 1],
           }}
-          className="absolute left-0 top-16 bottom-16 bg-gradient-to-r from-gray-900 to-gray-800 border-r-2 border-gray-700"
+          className="absolute left-0 top-16 bottom-16 bg-gray-900 border-r-2 border-gray-700"
           style={{
             clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
           }}
@@ -132,7 +132,7 @@ const PokedexShell = ({ children, loading }) => {
             duration: 1.5,
             ease: [0.22, 1, 0.36, 1],
           }}
-          className="absolute right-0 top-16 bottom-16 bg-gradient-to-l from-gray-900 to-gray-800 border-l-2 border-gray-700"
+          className="absolute right-0 top-16 bottom-16 bg-gray-900 border-l-2 border-gray-700"
           style={{
             clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
           }}
