bd3beb44d8d55e6cf5ecc1eb80bea5903313f71d
colombefioren <colomberakotonjanahary@gmail.com>
2025-07-20 08:37:02 +0300

    fix: padding on smaller screens

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,10 @@ const App = () => {
   return (
     <PokedexShell loading={loading}>
       <div className="h-full flex flex-col">
-        <div className="px-4 py-8">
+        <div className="px-4 py-5">
           <Search handleInput={handleInput} value={input} />
         </div>
-        <div className="flex-1 overflow-y-auto px-20 pb-8">
+        <div className="flex-1 overflow-y-auto px-10 pb-8 sm:px-20 sm:pb-8">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
             <Pokemons data={filteredPokemon} />
           </div>
